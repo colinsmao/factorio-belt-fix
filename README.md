ae502c5d7d2bcb93509d7ee33acde74d35e6ac94
colinsmao <colinsmao@gmail.com>
2024-10-29 11:27:05 -0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Reverts 'smart' belt building behaviour to 1.1 off state, ie does not build undergrounds when building past an obstacle.
+
+Cannot change the "build in a line" logic, since that isn't exposed to the modding interface.
+
---
+++ settings.lua
@@ -0,0 +1,15 @@
+
+data:extend({
+  {
+      type = "bool-setting",
+      name = "enabled",
+      setting_type = "runtime-global",
+      default_value = true
+  },
+  {
+      type = "bool-setting",
+      name = "replace-belts",
+      setting_type = "runtime-global",
+      default_value = false
+  },
+})
